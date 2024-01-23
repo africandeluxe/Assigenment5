151303d769bc640183fbebb7e27d5fec3c508004
Edwin Andermyr <andermyredwin@gmail.com>
2024-01-23 12:09:51 +0100

    restructured the JS files logic

--- js/company.js
+++ js/company.js
@@ -1,27 +1,13 @@
-
 $(() => {
-
-    function Company (
-        name,
-        role,
-        location,
-        job_description,
-        responsibilities,
-        requirements,
-        bonus,
-        smallimg, 
-        bigimg
-    ){
-        (this.name = name),
-        (this.role = role),
-        (this.location = location),
-        (this.job_description = job_description),
-        (this.responsibilities = responsibilities),
-        (this.requirements = requirements),
-        (this.bonus = bonus),
-        (this.smallimg = smallimg),
-        (this.bigimg = bigimg)
-    }
-
-})
-
+  function Company(name, role, location, job_description, responsibilities, requirements, bonus, smallimg, bigimg) {
+    (this.name = name),
+      (this.role = role),
+      (this.location = location),
+      (this.job_description = job_description),
+      (this.responsibilities = responsibilities),
+      (this.requirements = requirements),
+      (this.bonus = bonus),
+      (this.smallimg = smallimg),
+      (this.bigimg = bigimg);
+  }
+});
