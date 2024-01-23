7234ff04ab52502089671ac31599be0cd3f43672
Diyako-Qadri <qadri.diyako@gmail.com>
2024-01-23 11:50:56 +0100

    fixed conlfict

--- js/company.js
+++ js/company.js
@@ -1,32 +1,5 @@
-<<<<<<< HEAD
-$(() => {
-  //Hamburger menu 1st level/wokrks
-  $("nav .hamburger-menu").on("click", (event) => $(event.target).next("ul").toggle());
-  $("ul li").on({
-    mouseenter: (event) => $(event.target).toggleClass("active"),
-    mouseleave: (event) => $(event.target).toggleClass("active"),
-    click: (event) => {
-      $(event.target).find(".submenu").toggle("active");
-      $(event.target).siblings().find(".submenu").hide("active");
-    },
-  });
-  //hamburger 2nd level-works
-  $(".submenu li").on("click", (event) => $(event.target).next("ul").toggle());
-  $(".submenu li").on({
-    click: (event) => {
-      $(event.target).find(".submenu-2").toggle("active");
-      $(event.target).siblings().find(".submenu-2").hide("active");
-    },
-  });
-  //hamburger menu 3 level-works
-  $(".submenu-2 li").on("click", (event) => $(event.target).next("ul").toggle());
-  $(".submenu-2 li").on({
-    click: (event) => {
-      $(event.target).find(".submenu-3").toggle("active");
-      $(event.target).siblings().find(".submenu-3").hide("active");
-    },
-  });
 
+$(() => {
 
     function Company (
         name,
@@ -44,9 +17,7 @@ $(() => {
         (this.responsibilities = responsibilities),
         (this.requirements = requirements),
         (this.bonus = bonus)
-    }
-
-    
+    } 
 
 })
->>>>>>> constractor-company-diyako
+
