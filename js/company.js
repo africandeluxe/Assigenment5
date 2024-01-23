f8a79cc6a7cc2adb09ff7ee99d2b1a3f5d8981fd
Diyako-Qadri <qadri.diyako@gmail.com>
2024-01-23 11:44:34 +0100

    fixed compnay object

---
+++ js/company.js
@@ -0,0 +1,23 @@
+$(()=>{
+
+    function Company (
+        name,
+        role,
+        location,
+        job_description,
+        responsibilities,
+        requirements,
+        bonus
+    ){
+        (this.name = name),
+        (this.role = role),
+        (this.location = location),
+        (this.job_description = job_description),
+        (this.responsibilities = responsibilities),
+        (this.requirements = requirements),
+        (this.bonus = bonus)
+    }
+
+    
+
+})
