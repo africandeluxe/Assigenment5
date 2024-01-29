686f2b78cb44e66d783f1384796610236f09339a
Diyako-Qadri <qadri.diyako@gmail.com>
2024-01-29 15:30:16 +0100

    add style for student page and and it's looking like company page(not done)

--- js/general.js
+++ js/general.js
@@ -3,30 +3,38 @@ $(() => {
   //Hamburger menu 1st level/wokrks
 
   const navMenu = () => {
-    $("nav .hamburger-menu").on("click", (event) => $(event.target).next("ul").toggle());
-    $("ul li").on({
-      mouseenter: (event) => $(event.target).toggleClass("active"),
-      mouseleave: (event) => $(event.target).toggleClass("active"),
-      click: (event) => {
-        $(event.target).find(".submenu").toggle("active");
-        $(event.target).siblings().find(".submenu").hide("active");
+    $('nav .hamburger-menu').on('click', event =>
+      $(event.target).next('ul').toggle()
+    );
+    $('ul li').on({
+      mouseenter: event => $(event.target).toggleClass('active'),
+      mouseleave: event => $(event.target).toggleClass('active'),
+      click: event => {
+        $(event.target).find('.submenu').toggle('active');
+        $(event.target).siblings().find('.submenu').hide('active');
       },
     });
     //hamburger 2nd level-works
-    $(".submenu li").on("click", (event) => $(event.target).next("ul").toggle());
-    $(".submenu li").on({
-      click: (event) => {
-        $(event.target).find(".submenu-2").toggle("active");
-        $(event.target).siblings().find(".submenu-2").hide("active");
+    $('.submenu li').on('click', event => $(event.target).next('ul').toggle());
+    $('.submenu li').on({
+      click: event => {
+        $(event.target).find('.submenu-2').toggle('active');
+        $(event.target).siblings().find('.submenu-2').hide('active');
       },
     });
     //hamburger menu 3 level-works
-    $(".submenu-2 li").on("click", (event) => $(event.target).next("ul").toggle());
-    $(".submenu-2 li").on({
-      click: (event) => {
-        $(event.target).find(".submenu-3").toggle("active");
-        $(event.target).siblings().find(".submenu-3").hide("active");
+    $('.submenu-2 li').on('click', event =>
+      $(event.target).next('ul').toggle()
+    );
+    $('.submenu-2 li').on({
+      click: event => {
+        $(event.target).find('.submenu-3').toggle('active');
+        $(event.target).siblings().find('.submenu-3').hide('active');
       },
+    });
+
+    $('nav .hamburger-menu').on('click', () => {
+      $('.nav-container').toggleClass('added-padding');
     });
   };
 
