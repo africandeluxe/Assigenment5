82e6a3433aa21e8b0549b7841e5520d31fe260b4
Davide Baldi <d.baldi88@gmail.com>
2024-01-31 06:17:42 +0100

    backup profile done

--- js/miski.js
+++ js/miski.js
@@ -1,64 +1,82 @@
 $(() => {
-  function Staff(name, role, location, smallimg,) {
+  function Staff (name, role, location, email, img, map) {
     this.name = name;
     this.role = role;
     this.location = location;
-    this.smallimg = smallimg;
+    this.email= email;
+    this.img = img;
+    this.map= map;
   }
 
-  let Staff1 = new Staff(
-    "Miski Mohamed Ahmed",
-    "Operation Manager",
-    "Paris",
-    "./../img/companies-logo/InnovateTech Solutions-logos.jpeg",
-    "innovatetech_big.jpg"
-  );
+  let miski_object = new Staff(
+  "Miski Mohamed Ahmed",
+  "Operation Manager",
+  "Paris",
+  "miski@changemaker.com",
+  "./../img/our-team/beyonce.png",
+  `<iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.991626110944!2d2.2919063766770704!3d48.858370071332224!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e2964e34e2d%3A0x8ddca9ee380ef7e0!2sTorre%20Eiffel!5e0!3m2!1sit!2sse!4v1706678123344!5m2!1sit!2sse" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+  )
 
-  let ourStaff = [Staff1];//inserire in reset per ripristinare????
+  let edwin_object = new Staff(
+  "Edwin Andermyr",
+  "Operation Manager",
+  "Paris",
+  "edwin@changemaker.com",
+  "./../img/our-team/man.png",
+  `<iframe class= "map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2623.6408307283114!2d2.329676976678452!3d48.88412317133567!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e4eff6f0c89%3A0xf44e90af87482111!2sMoulin%20Rouge!5e0!3m2!1sit!2sse!4v1706616292265!5m2!1sit!2sse" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
 
-  const showStaffInfo = (Staffs) => {
+  )
+
+  let davide_object = new Staff(
+  "Davide Baldi",
+  "Operation Manager",
+  "Rome",
+  "davide@changemaker.com",
+  "./../img/our-team/mario.png",
+  `<iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2970.123074180057!2d12.48965597632581!3d41.8902101712405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132f61b6532013ad%3A0x28f1c82e908503c4!2sColosseo!5e0!3m2!1sit!2sse!4v1706615163021!5m2!1sit!2sse" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+  )
+
+  let diyako_object = new Staff(
+  "Diyako Qadri",
+  "Operation Manager",
+  "Stockholm",
+  "diyako@changemaker.com",
+  "./../img/our-team/johnny.png",
+  `<iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2036.259720179957!2d18.382925077276177!3d59.31191997460588!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f81272f2b3189%3A0xf965281351a280db!2sNordic%20Wellness%20V%C3%A4rmd%C3%B6%20Gustavsbergsv%C3%A4gen!5e0!3m2!1sit!2sse!4v1706617257553!5m2!1sit!2sse" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+  )
+
+  let robel_object = new Staff(
+  "Robel Girmay",
+  "Operation Manager",
+  "Stockholm",
+  "robel@changemaker.com",
+  "./../img/our-team/will.png",
+  `<iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2035.2953199697615!2d18.086525494426628!3d59.328023209157074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f9d546d8329af%3A0xcff09af1b4c13241!2sRegalskeppet%20Vasa!5e0!3m2!1sit!2sse!4v1706616755280!5m2!1sit!2sse" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+  )
+
+  let staff = miski_object;
+
+  const showStaffInfo = (staff) => {
     const staffContent = $('.main-content').empty();
-    Staffs.forEach((Staff) => {
+    
       const profile= $(".main-content").append(`
-      <div class="company-container">
-        <img class="company-img" src="${Staff1.smallimg}" alt="${Staff1.name}">
-        <div class="company-text">  
-        <h2 class="company-title"> ${Staff1.name} </h2> 
-        <p class="company-location"> Location ${Staff1.location} </p> 
+      <div class="staff-container">
+        <img class="staff-img" src="${staff.img}" alt="${staff.name}">
+        <h1 class="staff-text">${staff.name} </h1>  
+        <h3 class="staff-role"> ${staff.role} </h3>
+        <p class="staff-email"> Email: ${staff.email} </p> 
+        <p class="staff-location"> Location ${staff.location} </p> 
+        ${staff.map}
         </div> 
       </div>
     `)
-    $(".main-content").find('.read-more').on('click', () => {
-      const modalContent = $(`
-        <div class="modal" id="modal">
-          <div class="modal-header">
-          <img class="company-img" src="${Staff1.smallimg}" alt="${Staff1.name}">
-            <div class="title">${Staff1.name} </div>
-            <button class="close-button">&times;</button>
-          </div>
-          <div class="modal-body">
-          <h3>${Staff1.role}</h3>
-          </div>
-        </div>
-        <div class="active" id="overlay"></div>
-      `);
+    
 
-      modalContent.find('.close-button, #overlay').on('click', () => {
-        modalContent.removeClass('active');
-        $('#overlay').removeClass('active');
-        showStaffInfo(ourStaffs);
-      });
+    $(".about-me").append(profile);
 
-      staffContent.empty().append(modalContent);
-      modalContent.addClass('active');
-      $('#overlay').addClass('active');
-    });
-
-    companyContent.append(profile);
-
-    });
   };
+  showStaffInfo(staff);
 
-  showCompanyInfo(ourCompanies); 
+  
 
 });
